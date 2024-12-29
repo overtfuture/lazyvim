8c166cc89a156a9eb46391b9eb941231bedc9744
Nathan Grey <nate@grey.bz>
2024-12-29 14:59:56 -0500

    Theme and quality of life updates

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,11 +1,17 @@
 return {
   { "Mofiqul/dracula.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "tokyonight-storm",
     },
   },
 }
