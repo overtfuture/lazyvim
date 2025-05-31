78bedaf0ff4783a3533a789409e181c272e9e28f
Nathan Grey <nate@grey.bz>
2025-05-31 10:23:24 -0400

    Removes shortcut for esc to quit windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 -- ToggleTerm Keymapping
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
   vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
